733054904986c9c864e700d76563e76b45052d61
awlana04 <cawlana040@gmail.com>
2020-12-26 15:18:02 -0300

    mobile

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Text>Hello World! SignIn page.</Text>
+    </Container>
+  );
+};
+
+export default SignIn;
