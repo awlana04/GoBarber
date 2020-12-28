adb2b8961ea44d2c9f1b3bdd7870533b97bfc5be
awlana04 <cawlana040@gmail.com>
2020-12-27 22:55:14 -0300

    DDD

---
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,8 @@
+import Appointment from '@appointmentsEntitie/Appointment';
+
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+
+export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointment | undefined>;
+}
