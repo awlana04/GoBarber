e676d0e67b912cd8c77e307d086de7888d97e328
awlana04 <cawlana040@gmail.com>
2020-12-31 17:25:20 -0300

    tokens

---
+++ backend/src/modules/users/providers/index.ts
@@ -0,0 +1,7 @@
+import { container } from 'tsyringe';
+
+import IHashProvider from './HashProvider/models/IHashProvider';
+
+import BCryptHashProvider from './HashProvider/implemetations/BCryptHashProvider';
+
+container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import appointmentsRouter from '@appointmentsRoutes/appointments.routes';
 import usersRouter from '@usersRoutes/users.routes';
 import sessionsRouter from '@usersRoutes/sessions.routes';
+import passwordRouter from '@usersRoutes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
