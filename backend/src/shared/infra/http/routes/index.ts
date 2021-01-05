a23cf54e07c58d61151f2f54f870d3e5fb698853
awlana04 <cawlana040@gmail.com>
2021-01-04 21:54:58 -0300

    providers route and controller

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import appointmentsRouter from '@appointmentsRoutes/appointments.routes';
+import providersRouter from '@appointmentsRoutes/providers.routes';
 import usersRouter from '@usersRoutes/users.routes';
 import sessionsRouter from '@usersRoutes/sessions.routes';
 import passwordRouter from '@usersRoutes/password.routes';
@@ -9,6 +10,7 @@ import profileRouter from '@usersRoutes/profile.routes';
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
