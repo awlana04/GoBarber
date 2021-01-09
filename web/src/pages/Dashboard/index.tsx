0d56be14cf6ed664ad0c9a012d212dbac7a78f61
awlana04 <cawlana040@gmail.com>
2021-01-09 18:33:37 -0300

    dashboard header

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Hello World! Dashboard.</h1>;
+import Logo from '../../assets/logo.svg';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={Logo} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src="https://avatars2.githubusercontent.com/u/65423909?s=88&u=50e58e511e404791c688d4638ac210855aebc05a&v=4"
+              alt="Awlana Costa"
+            />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>Awlana Costa</strong>
+            </div>
+          </Profile>
+
+          <button type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Header = styled.header`
+  padding: 32px 0;
+
+  background: #28262e;
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+`;
+
+export const Profile = styled.div``;
