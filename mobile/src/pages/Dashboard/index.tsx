8f0b60c05b47893fa575079b2a2d087885f4c363
awlana04 <cawlana040@gmail.com>
2021-01-14 15:44:51 -0300

    dashboard providersList mobile

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather'
 
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 import {
   Container,
@@ -10,15 +12,41 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
+  ProvidersListTitle,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const { navigate } = useNavigation();
 
   const { signOut, user } = useAuth();
 
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
+  }, [navigate]);
+
+  const navigateToCreateAppointment = useCallback((providerId: string) => {
+    navigate('CreateAppointment', { providerId });
   }, [navigate]);
 
   return (
@@ -33,6 +61,37 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        renderItem={({ item: provider }) => (
+          <ProviderContainer
+            onPress={() => navigateToCreateAppointment(provider.id)}
+          >
+            <ProviderAvatar source={{ uri: provider.avatar_url }} />
+
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+
+                <ProviderMetaText>Segunda à sexta</ProviderMetaText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+
+                <ProviderMetaText>8h às 18h</ProviderMetaText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
+      />
     </Container>
     );
   }
--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,4 +1,8 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import { Provider } from './index';
 
 export const Container = styled.View`
   flex: 1;
@@ -35,3 +39,58 @@ export const UserAvatar = styled.Image`
   border-radius: 28px;
 `;
 
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px 16px;
+`;
+
+export const ProvidersListTitle = styled.Text`
+  margin-bottom: 24px;
+
+  color: #f4ede8;
+  font-size: 24px;
+  font-family: 'RobotoSlab_500Medium'
+`;
+
+export const ProviderContainer = styled(RectButton)`
+  margin-bottom: 16px;
+  padding: 20px;
+
+  background: #3e3b47;
+  border-radius: 10px;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 72px;
+  height: 72px;
+
+  border-radius: 36px;
+`;
+
+export const ProviderInfo = styled.View`
+  margin-left: 20px;
+
+  flex: 1;
+`;
+
+export const ProviderName = styled.Text`
+  color: #f4ede8;
+  font-size: 18px;
+  font-family: 'RobotoSlab_500Medium';
+`;
+
+export const ProviderMeta = styled.View`
+  margin-top: 8px;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProviderMetaText = styled.Text`
+  margin-left: 8px;
+
+  color: #999591;
+  font-family: 'RobotoSlab_400Regular';
+`;
