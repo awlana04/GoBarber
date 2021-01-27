8b03d1fc433de5504af3373e7559575a448ed0d9
awlana04 <cawlana040@gmail.com>
2021-01-26 23:30:29 -0300

    mobile

--- mobile/src/pages/CreateAppointment/styles.ts
+++ mobile/src/pages/CreateAppointment/styles.ts
@@ -1,3 +1,81 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+import { Provider } from './index';
+
+interface ProviderContainerProps {
+  selected: boolean;
+}
+
+interface ProviderNameProps {
+  selected: boolean;
+}
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  margin-left: 16px;
+
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  margin-left: auto;
+
+  border-radius: 28px;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  margin-right: 16px;
+  padding: 8px 12px;
+
+  background: ${(props) => (props.selected ? '#ff9000' : '#3e3b47')};
+  border-radius: 10px;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  margin-left: 8px;
+
+  color: ${(props) => (props.selected ? '#232129' : '#f4ede8')};
+  font-size: 16px;
+  font-family: 'RobotoSlab-Medium';
+`;
